2d02ed5ab5057efe807082395ec7fdc35be6a8fa
ad-szwed <shvedatech@gmail.com>
2021-09-07 10:45:53 +0100

    function refactoring

--- filter/removeOddNumbers.js
+++ filter/removeOddNumbers.js
@@ -1,19 +1,13 @@
-const removeOddNumbers = arr => {
-    // let evenNumbers = [];
+const removeOddNumbers = arr => arr.filter(number => number % 2 === 0);
+// let evenNumbers = [];
 
-    // for (let i = 0; i < arr.length; i++) {
-    //     if (arr[i] % 2 === 0) {
-    //         evenNumbers.push(arr[i]);
-    //     }
-    // }
-    // return evenNumbers;
-    return arr.filter(elem => elem % 2 === 0);
-}
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 === 0) {
+//         evenNumbers.push(arr[i]);
+//     }
+// }
+// return evenNumbers;
+
+
 
 export default removeOddNumbers;
-
-
-// var arr = [1, 2, 3, 4, 5, 6]
-// removeOddNumbers(arr) => [2, 4, 6]
-// removeOddNumbers([]) => []
-// removeOddNumbers([2, 4, 6, 8]) => [2, 4, 6, 8]
